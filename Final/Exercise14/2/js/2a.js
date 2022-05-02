19a0de6e70738733a279fd6bc4a1f3162261c8ee
Mateo, Jeremiah <98523158+JeremiahMateo@users.noreply.github.com>
2022-05-02 11:20:28 +0400

    Add files via upload

--- Final/Exercise14/2/js/2a.js
+++ Final/Exercise14/2/js/2a.js
@@ -1,9 +1,8 @@
-function myfunction(){
     document.getElementById("target").innerHTML="Mateo, Jeremiah Ezra E.<br>"+
     "BS Computer Science<br>"+
     "1st Year<br>"+
     "HUMCOM1-IAA2<br>"+
-    "Human Computer Interaction"
-}
+    "Human Computer Interaction";
+
 
     
