dc1cf531a31a741aa3a6f5355baf4388595b6f60
Mateo, Jeremiah <98523158+JeremiahMateo@users.noreply.github.com>
2022-05-13 19:56:26 +0400

    Add files via upload

---
+++ Final/Exercise15/Script15.js
@@ -0,0 +1,24 @@
+function fn1() {
+    
+    var fname =document.getElementById("fname").value;
+    var age =document.getElementById("age").value;
+    var tel =document.getElementById("tel").value;
+    var email =document.getElementById("email").value;
+    var mail_add =document.getElementById("mail-add").value;
+    var program =document.getElementById("program").value;
+    var idNum =document.getElementById("id-num").value;
+    var yrLvl =document.getElementById("yr-lvl").value;
+    var section =document.getElementById("section").value;
+    document.write(fname);
+    document.write("<br>"+age);
+    document.write("<br>"+tel);
+    document.write("<br>"+email);
+    document.write("<br>"+mail_add);
+    document.write("<br>"+program);
+    document.write("<br>"+idNum);
+    document.write("<br>"+yrLvl);
+    document.write("<br>"+section);
+}
+
+
+// console.log("Hello");
